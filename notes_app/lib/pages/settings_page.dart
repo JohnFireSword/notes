b4b7093742d3bc83c1e9416a2a3d92fe21c24b81
John Giotis <johngio20@gmail.com>
2025-03-12 10:20:42 +0200

    jj

--- notes_app/lib/pages/settings_page.dart
+++ notes_app/lib/pages/settings_page.dart
@@ -19,11 +19,7 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
+     
       body: Column(
         children: [
           Container(
